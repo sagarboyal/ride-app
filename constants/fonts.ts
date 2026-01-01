594b836fd2b81ceda52e13a0f3cbe925c15c4447
sagarboyal <sboyal12@gmail.com>
2026-01-01 11:46:07 +0530

    outfit fonts

---
+++ constants/fonts.ts
@@ -0,0 +1,13 @@
+import {
+    Outfit_400Regular,
+    Outfit_500Medium,
+    Outfit_600SemiBold,
+    Outfit_700Bold,
+} from "@expo-google-fonts/outfit";
+
+export const fonts = {
+  Outfit_400Regular,
+  Outfit_500Medium,
+  Outfit_600SemiBold,
+  Outfit_700Bold,
+};
