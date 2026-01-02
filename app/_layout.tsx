92ad1d17660e5e1e3da2668c21e897a752a91676
sagarboyal <sboyal12@gmail.com>
2026-01-02 19:27:16 +0530

    config custom font

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,11 @@
+import { fontAssets } from "@/src/constants/fonts";
+import { useFonts } from "@expo-google-fonts/outfit";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts(fontAssets);
+
+  if (!fontsLoaded) return null;
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" />
--- src/constants/fonts.ts
+++ src/constants/fonts.ts
@@ -1,13 +1,35 @@
 import {
-    Outfit_400Regular,
-    Outfit_500Medium,
-    Outfit_600SemiBold,
-    Outfit_700Bold,
-} from "@expo-google-fonts/outfit";
-
-export const fonts = {
+  Outfit_100Thin,
+  Outfit_200ExtraLight,
+  Outfit_300Light,
   Outfit_400Regular,
   Outfit_500Medium,
   Outfit_600SemiBold,
   Outfit_700Bold,
+  Outfit_800ExtraBold,
+  Outfit_900Black
+} from "@expo-google-fonts/outfit";
+
+export const fontAssets = {
+  Outfit_100Thin,
+  Outfit_200ExtraLight,
+  Outfit_300Light,
+  Outfit_400Regular,
+  Outfit_500Medium,
+  Outfit_600SemiBold,
+  Outfit_700Bold,
+  Outfit_800ExtraBold,
+  Outfit_900Black
+};
+
+export const fonts = {
+  thin: "Outfit_100Thin",
+  extraLight: "Outfit_200ExtraLight",
+  light: "Outfit_300Light",
+  regular: "Outfit_400Regular",
+  medium: "Outfit_500Medium",
+  semiBold: "Outfit_600SemiBold",
+  bold: "Outfit_700Bold",
+  extraBold: "Outfit_800ExtraBold",
+  black: "Outfit_900Black"
 };
